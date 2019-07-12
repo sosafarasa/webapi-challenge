f5e308ad330ffa6cbb7859dc6f01b19a0aa503cc
Agustina Sosa <agustinasf85@gmail.com>
2019-07-12 13:45:00 -0400

    Created post endpoints for projects and actions

--- data/actionsRouter.js
+++ data/actionsRouter.js
@@ -12,17 +12,14 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/:id', async (req, res) => {
+router.post('/', async (req, res) => {
     try{
-        const action = await Actions.get(req.params.id);
-        if(action){
-            res.status(200).json(action);
-        } else {
-            res.status(404).json({ message: 'The action requested does not exist.' });
-        }
+        const addAction = await Actions.insert(req.body);
+        res.status(200).json({ message: 'Action added!' })
     } catch(err) {
-        res.status(500).json({ error: 'The action requested could not be retrieved.' });
+        res.status(500).json({ message: 'Action could not be added.' });
     }
 });
+
 
 module.exports = router;
--- data/projectsRouter.js
+++ data/projectsRouter.js
@@ -25,4 +25,13 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+router.post('/', async (req, res) => {
+    try {
+        const addProject = await Projects.insert(req.body);
+        res.status(200).json({ message: 'Project added!'});
+    } catch(err) {
+        res.status(500).json({ message: 'Project could not be added.' })
+    }
+});
+
 module.exports = router;
